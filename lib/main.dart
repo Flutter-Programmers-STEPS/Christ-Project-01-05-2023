bbfac002750c06a1e4566bf6a4f5d7ea1e0635e4
Flutter-Programmers-STEPS <126236832+Flutter-Programmers-STEPS@users.noreply.github.com>
2023-06-01 15:57:51 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp_christ/helper/todoapp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.green,
+      ),
+      home: TodoApp(),
+    );
+  }
+}
